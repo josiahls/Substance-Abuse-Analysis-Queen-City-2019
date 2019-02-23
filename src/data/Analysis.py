2875ad5c7a6219bfef696bd0f6472b29b3abdc50
j.parker.garrison@gmail.com <j.parker.garrison@gmail.com>
2019-02-23 05:23:26 -0500

    add dec vars

--- src/data/Analysis.py
+++ src/data/Analysis.py
@@ -12,6 +12,8 @@ Decision Variables Variables whose values we would like to minimize.
 
 class ParkerAnalysis:
     IGNORED_VARIABLES = ['YEAR', 'CASEID', 'CBSA', 'STFIPS']
+    INDEX_VARIABLES = ['CASEID']
+    DECISION_VARIABLES = ['REASON', 'LOS']
 
     """ Cleaning VARIABLES """
     CATEGORICAL_VARIABLES = ['DSMCRIT', 'RACE', 'MARSTAT', 'LIVARAG', 'PRIMINC', 'REGION', 'DIVISION', 'SERVSETD', 'REASON', 'DETCRIM', 'SUB1', 'ROUTE1', 'SUB2', 'ROUTE2', 'SUB3', 'ROUTE3', 'IDU', 'ALCDRUG', 'DSMCRIT', 'HLTHINS', 'PRIMPAY']
@@ -20,6 +22,7 @@ class ParkerAnalysis:
     CONTINUOUS_VARIABLES += [e for e in CONTINUOUS_AND_1HOT_VARIABLES]
     BOOLEAN_VARIABLES = ['SEX', 'PREG', 'VET', 'METHUSE', 'ALCFLG', 'COKEFLG', 'MARFLG', 'HERFLG', 'METHFLG', 'OPSYNFLG', 'PCPFLG', 'HALLFLG', 'MTHAMFLG', 'AMPHFLG', 'STIMFLG', 'BENZFLG', 'TRNQFLG', 'BARBFLG', 'SEDHPFLG', 'INHFLG', 'OTCFLG', 'OTHERFLG', 'PSYPROB'] # exactly 2 values that need to be condensed to one
     CONVERT_TO_BOOLEAN_VARIABLES = {'ETHNICITY': 5}
+    CATEGORICAL_VARIABLES += [e for e in CONVERT_TO_BOOLEAN_VARIABLES]
     #CUSTOM_VARIABLES = ['DSMCRIT'] # these will be passed to a function with the name of the variable for cleaning
     # geo variable: STFIPS, CBSA
     # look at EMPLOY/DETNLF
@@ -27,7 +30,7 @@ class ParkerAnalysis:
     # ignored STFIPS, CBSA
     # further break down: SERVSETD
     # LOS needs to be changed to the correct day range
-    # more frequency: FREQ1 and FREQ2
+    # more frequency: FREQ1, FREQ2, FREQ3
     # FRSTUSE1 is a continuous and 1 hot, but ignore the 0.2%
     # ALCDRUG should be split
     # DSMCRIT could be split
