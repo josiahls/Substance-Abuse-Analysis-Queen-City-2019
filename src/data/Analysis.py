09cca1122ffec39831cc1f891a919ec4bc06c87d
j.parker.garrison@gmail.com <j.parker.garrison@gmail.com>
2019-02-23 05:04:05 -0500

    add all variables for Parker

--- src/data/Analysis.py
+++ src/data/Analysis.py
@@ -11,18 +11,24 @@ Decision Variables Variables whose values we would like to minimize.
 """
 
 class ParkerAnalysis:
-    IGNORED_VARIABLES = ['YEAR', 'CASEID']
-    FIXED_VARIABLES = []
-    DYNAMIC_VARIABLES = []
-    DECISION_VARIABLES = ['REASON', 'LOS']
-    HARD_NOT_NAN_VARIABLES = [] + DECISION_VARIABLES
 
     """ Cleaning VARIABLES """
-    CATEGORICAL_VARIABLES = ['DSMCRIT']
-    CONTINUOUS_AND_1HOT_VARIABLES = {'AGE': ['12']} # the value of 12 in age needs an additional 1hot column
-    CONTINUOUS_VARIABLES = []
-    BOOLEAN_VARIABLES = ['SEX']
+    CATEGORICAL_VARIABLES = ['DSMCRIT', 'RACE', 'MARSTAT', 'LIVARAG', 'PRIMINC', 'REGION', 'DIVISION', 'SERVSETD', 'REASON', 'DETCRIM', 'SUB1', 'ROUTE1', 'SUB2', 'ROUTE2', 'SUB3', 'ROUTE3', 'IDU', 'ALCDRUG', 'DSMCRIT', 'HLTHINS', 'PRIMPAY']
+    CONTINUOUS_AND_1HOT_VARIABLES = {'AGE': ['12'], 'ARRESTS': ['2'], 'DAYWAIT': ['X'], 'NOPRIOR': ['5'] } # the value of 12 in age needs an additional 1hot column
+    CONTINUOUS_VARIABLES = ['EDUC', 'EMPLOY', 'DETNLF', 'LOS', 'FREQ1', 'FRSTUSE1', 'FREQ2', 'FRSTUSE2', 'FREQ3', 'FRSTUSE3', 'NUMSUBS']
+    BOOLEAN_VARIABLES = ['SEX', 'PREG', 'VET', 'METHUSE', 'ALCFLG', 'COKEFLG', 'MARFLG', 'HERFLG', 'METHFLG', 'OPSYNFLG', 'PCPFLG', 'HALLFLG', 'MTHAMFLG', 'AMPHFLG', 'STIMFLG', 'BENZFLG', 'TRNQFLG', 'BARBFLG', 'SEDHPFLG', 'INHFLG', 'OTCFLG', 'OTHERFLG', 'PSYPROB'] # exactly 2 values that need to be condensed to one
+    CONVERT_TO_BOOLEAN_VARIABLES = {'ETHNICITY': 5}
     #CUSTOM_VARIABLES = ['DSMCRIT'] # these will be passed to a function with the name of the variable for cleaning
+    # geo variable: STFIPS, CBSA
+    # look at EMPLOY/DETNLF
+
+    # STFIPS, CBSA
+    # further break down: SERVSETD
+    # LOS needs to be changed to the correct day range
+    # more frequency: FREQ1 and FREQ2
+    # FRSTUSE1 is a continuous and 1 hot, but ignore the 0.2%
+    # ALCDRUG should be split
+    # DSMCRIT could be split
 
 class JosiahAnalysis:
     IGNORED_VARIABLES = ['YEAR', 'CASEID', 'CBSA']
